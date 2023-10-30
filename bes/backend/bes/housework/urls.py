b1ebd223aecb858d3438ca31e6b3b4b75c9917d3
Lucile Lévêque <lululeveq@hotmail.fr>
2023-10-30 17:42:14 +0100

    URLs and views implemented for houses API calls (test OK)

--- bes/backend/bes/housework/urls.py
+++ bes/backend/bes/housework/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 #Available routes
 urlpatterns = [
-    
+    path('api/houses/', views.house_list),
+    path('api/houses/<int:pk>/', views.house_detail),
 ]
--- bes/backend/bes/housework/views.py
+++ bes/backend/bes/housework/views.py
@@ -1,3 +1,52 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+from .models import House, HouseworkPossibleTask, HouseworkMadeTask, History
+from .serializers import HouseSerializer, HouseworkPossibleTaskSerializer, HouseworkMadeTaskSerializer, HistorySerializer
 
-# Create your views here.
+
+@csrf_exempt
+def house_list(request):
+    """
+    List all houses, or create a new house.
+    """
+    if request.method == 'GET':
+        houses = House.objects.all()
+        serializer = HouseSerializer(houses, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = HouseSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+    
+
+@csrf_exempt
+def house_detail(request, pk):
+    """
+    Retrieve, update or delete a code house.
+    """
+    try:
+        house = House.objects.get(pk=pk)
+    except House.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        serializer = HouseSerializer(house)
+        return JsonResponse(serializer.data)
+
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = HouseSerializer(house, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+
+    elif request.method == 'DELETE':
+        house.delete()
+        return HttpResponse(status=204)
