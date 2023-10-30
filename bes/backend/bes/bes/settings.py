447f45137d2f5f1beb8a3e73e973960af0caa68b
Lucile Lévêque <lululeveq@hotmail.fr>
2023-10-30 17:38:41 +0100

    Django-Restplus added

--- bes/backend/bes/bes/settings.py
+++ bes/backend/bes/bes/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'housework.apps.HouseworkConfig',
     'account.apps.AccountConfig',
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
