88080d5d51ed0ce3dd8b9fdf6fa5958ee9044e00
tgalton <galtontom@gmail.com>
2024-06-05 14:51:40 +0200

    effect for userinfo in hearth

--- frontend/src/app/store/effects/hearths.effects.ts
+++ frontend/src/app/store/effects/hearths.effects.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { of } from 'rxjs';
-import { catchError, map, switchMap } from 'rxjs/operators';
+import { catchError, exhaustMap, map, switchMap } from 'rxjs/operators';
+import { UserService } from 'src/app/services/user.service';
 import { HearthService } from '../../services/hearth.service';
 import * as HearthActions from './../actions/hearths.actions';
 
@@ -9,18 +10,41 @@ import * as HearthActions from './../actions/hearths.actions';
 export class HearthEffects {
   constructor(
     private actions$: Actions,
-    private hearthService: HearthService
+    private hearthService: HearthService,
+    private userService: UserService
   ) {}
 
   loadHearths$ = createEffect(() => {
     return this.actions$.pipe(
       ofType(HearthActions.loadHearths),
-      switchMap((action) => {
-        return this.hearthService.getHearthsByUser(action.userId).pipe(
-          map((hearths) => HearthActions.loadHearthsSuccess({ hearths })),
+      switchMap((action) =>
+        this.hearthService.getHearthsByUser(action.userId).pipe(
+          // First, map to get all unique user IDs across hearths
+          exhaustMap((hearths) => {
+            const userIds: number[] = hearths
+              .reduce((acc, hearth) => acc.concat(hearth.users), [] as number[])
+              .filter((value, index, self) => self.indexOf(value) === index); // unique user IDs
+
+            return this.userService.getUsersAvatarsAndNames(userIds).pipe(
+              map((userInfos) => {
+                // Combine user infos with hearths
+                const enrichedHearths = hearths.map((hearth) => ({
+                  ...hearth,
+                  userInfos: hearth.users.map((id) =>
+                    userInfos.find(
+                      (info) => info.split(':')[0] === id.toString()
+                    )
+                  ),
+                }));
+                return HearthActions.loadHearthsSuccess({
+                  hearths: enrichedHearths,
+                });
+              })
+            );
+          }),
           catchError((error) => of(HearthActions.loadHearthsFailure({ error })))
-        );
-      })
+        )
+      )
     );
   });
 }
