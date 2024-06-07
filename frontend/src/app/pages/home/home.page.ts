9a4f1ab4418bb1575d9d65c6eb273167e68f828f
tgalton <galtontom@gmail.com>
2024-06-08 00:20:57 +0200

    correction scss background color

--- frontend/src/app/pages/home/home.page.ts
+++ frontend/src/app/pages/home/home.page.ts
@@ -1,14 +1,34 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterModule } from '@angular/router';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonGrid, IonRow, IonCol, IonButton } from '@ionic/angular/standalone';
-import { HeaderComponent } from "../../shared/header/header.component"
+import { RouterModule } from '@angular/router';
+import {
+  IonButton,
+  IonCol,
+  IonContent,
+  IonGrid,
+  IonHeader,
+  IonRow,
+  IonTitle,
+  IonToolbar,
+} from '@ionic/angular/standalone';
+import { HeaderComponent } from '../../shared/header/header.component';
 
 @Component({
-    selector: 'app-home',
-    templateUrl: 'home.page.html',
-    styleUrls: ['home.page.scss'],
-    standalone: true,
-    imports: [IonButton, IonCol, IonRow, IonGrid, IonHeader, IonToolbar, IonTitle, IonContent, RouterModule, HeaderComponent]
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+  standalone: true,
+  imports: [
+    IonButton,
+    IonCol,
+    IonRow,
+    IonGrid,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    RouterModule,
+    HeaderComponent,
+  ],
 })
 export class HomePage {
   constructor() {}
